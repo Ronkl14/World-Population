95f126970ad84bb2f7026caca6e2883a1757e387
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-02-09 23:06:43 +0200

    getCountriesByContinent now returns object

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 const btn = document.querySelectorAll(".btn");
 
 async function getCountriesByContinent(continent) {
-  const continentRes = await fetch(
-    `https://restcountries.com/v3.1/region/${continent}`
-  );
-
-  if (!continentRes.ok) {
-    throw new Error("Something went wrong :(");
-  }
-
   try {
-    const countries = await continentRes.json();
-    const countryNames = countries.map((country) => country.name.common);
-    const countryPopulation = countries.map((country) => country.population);
+    const continentRes = await fetch(
+      `https://restcountries.com/v3.1/region/${continent}`
+    );
 
-    console.log(countryNames);
-    console.log(countryPopulation);
+    if (!continentRes.ok) {
+      throw new Error("Something went wrong :(");
+    }
+
+    const countries = await continentRes.json();
+    let countriesObj = [];
+    countries.forEach((country, idx) => {
+      countriesObj[idx] = {};
+      countriesObj[idx].name = country.name.common;
+    });
+    countries.forEach(
+      (country, idx) => (countriesObj[idx].population = country.population)
+    );
+    console.log(countriesObj);
+    return countriesObj;
   } catch (error) {
     console.log(error);
   }
 }
+
+// async function getResultFromPromise(callbackFn) {
+//   const result = await callbackFn;
+//   console.log(result);
+// }
+
+getCountriesByContinent("africa").then((a) => console.log(a));
+// getResultFromPromise(getCountriesByContinent("americas"));
