25f8e1f4fd272364fe07309e01762d6b154b5c48
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-02-10 14:42:55 +0200

    added chart function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const btn = document.querySelectorAll(".btn");
+const ctx = document.getElementById("myChart");
 
-async function getCountriesByContinent(continent) {
+async function displayCountriesByContinent(continent) {
   try {
     const continentRes = await fetch(
       `https://restcountries.com/v3.1/region/${continent}`
@@ -19,17 +20,49 @@ async function getCountriesByContinent(continent) {
     countries.forEach(
       (country, idx) => (countriesObj[idx].population = country.population)
     );
-    console.log(countriesObj);
-    return countriesObj;
+
+    countryNames = countriesObj.map((country) => country.name);
+    countryPopulations = countriesObj.map((country) => country.population);
+    console.log(countryNames);
+    console.log(countryPopulations);
+
+    createChart(ctx, countryNames, countryPopulations);
   } catch (error) {
     console.log(error);
   }
 }
 
-// async function getResultFromPromise(callbackFn) {
-//   const result = await callbackFn;
-//   console.log(result);
-// }
+displayCountriesByContinent("asia");
 
-getCountriesByContinent("africa").then((a) => console.log(a));
-// getResultFromPromise(getCountriesByContinent("americas"));
+// add class with continent name to each btn
+// every btn gets an eventlistener triggering the continet func with the corresponding continent
+//getCountriesByContinent returns an object of countries
+// change it to create arrays, or just use map on object?
+// function that will create chart
+//function that will insert buttons of each country
+//create async function to get cities
+//add eventlistener to trigger each button to get corresponding cities
+// use chart functino created
+
+function createChart(chartDOM, labelArr, dataArr) {
+  new Chart(chartDOM, {
+    type: "bar",
+    data: {
+      labels: labelArr,
+      datasets: [
+        {
+          label: "# of Votes",
+          data: dataArr,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
